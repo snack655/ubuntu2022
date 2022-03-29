8e45125f929914e793e07533e719c96b9dbf89ca
snack655 <cmjcmj1230@gmail.com>
2022-03-29 12:06:17 +0900

    Add : init file

---
+++ ex0329/ex01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define MAXLINE 100
+
+// 문자열을 입력 받아 긴 문자열 일 때 출력하는 프로그래밍
+void copy(char from[], char to[]);
+char line[MAXLINE];
+char instr[MAXLINE];
+
+int main() {
+	
+
+	// 정상 종료 일 때는 0을 리턴한다.
+	return 0;
+}
+
+// u 뒤로 가기 ctrl+r 앞으로 가기
+// ctrl + z ps 프로세스
+// fg 앞으로 다시
+// bg
+void copy(char from[], char to[]);
+}
