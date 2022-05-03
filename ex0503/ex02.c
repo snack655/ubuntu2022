acebc199f5b2aa9e2ec5b30b358e5c25930eee31
snack655 <cmjcmj1230@gmail.com>
2022-05-03 17:52:48 +0900

    Study : directory program

---
+++ ex0503/ex02.c
@@ -0,0 +1,32 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+    // 디렉토리 엔트리
+    DIR *dp;
+    // 디렉토리, 파일 담는 곳
+    struct dirent *d;
+
+    char directoryname[512];
+    if (argc == 1) {
+        strcpy(directoryname, ".");
+    } else {
+        strcpy(directoryname, argv[1]);
+    }
+    printf("directoryname = %s\n", directoryname);
+
+    dp = opendir(directoryname);
+    if (dp == NULL) {
+        perror("dp is null");
+        exit(0);
+    }
+    while ( (d = readdir(dp)) != NULL) {
+        printf("d -> d_name %s\n", d -> d_name);
+    } 
+
+    return 0;
+}
