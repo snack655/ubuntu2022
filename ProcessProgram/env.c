de4b2c4b93ea3ffa2764d225707d2355fd257e44
snack655 <cmjcmj1230@gmail.com>
2022-06-05 17:41:16 +0900

    Modify : env select flow

--- ProcessProgram/env.c
+++ ProcessProgram/env.c
@@ -2,12 +2,29 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    char **ptr;
+    char answer;
+    char *ptr;
+    char **ptrs;
+    char keyward[100];
     extern char **environ;
 
-    // 모든 환경 변수를 출력
-    for (ptr = environ; *ptr != 0; ptr++)
-        printf("%s \n", *ptr);
+    printf("모든 환경 변수를 출력하시겠습니까? : ");
+    scanf("%c", &answer);
+    if(answer == 'y' || answer == 'Y') {
+        // 모든 환경 변수를 출력
+        for (ptrs = environ; *ptrs != 0; ptrs++)
+            printf("%s \n", *ptrs);
 
+        exit(0);
+    }
+
+    printf("특정 환경 변수를 출력합니다.\n출력하실 환경변수를 입력해주세요 : ");
+    scanf("%s", keyward);
+    ptr = getenv(keyward);
+    if(ptr == NULL) {
+        printf("%s라는 환경변수가 없습니다.", keyward);
+        exit(0);
+    }
+    printf("%s = %s", keyward, ptr);
     exit(0);
 }
